34c0bd7d74f8c6075c7f288088e25f884ab44a36
Dan Drinkard <dan.drinkard@gmail.com>
2011-09-20 22:40:14 -0400

    update uuid generator

--- models.js
+++ models.js
@@ -1,13 +1,16 @@
-var uuid = require('uuid')
-  , hash = require('hashlib');
+var uuid = require('uuid-lib');
+
+function getId(){
+  return uuid.create().value;
+}
 
 module.exports.User = function(mongoose){
   var Schema = mongoose.Schema
    ,  User = new Schema({
           email: { type: String, match: /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/, unique: true}
-        , key: { type: String, default: hash.md5, index: true }
+        , key: { type: String, default: getId, index: true }
         , activated: { type: Boolean, default: false, index: true }
-        , activation_hash: { type: String, default: uuid.generate, index: true }
+        , activation_hash: { type: String, default: getId, index: true }
         , created_at: { type: Date, default: Date.now }
       });
   return mongoose.model('User', User);
